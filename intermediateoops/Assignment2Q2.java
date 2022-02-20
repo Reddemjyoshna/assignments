12e95677a0967b786ead5665a158570036faaa10
Reddemjyoshna <62939217+Reddemjyoshna@users.noreply.github.com>
2022-02-20 16:29:24 +0530

    Add files via upload

---
+++ intermediateoops/Assignment2Q2.java
@@ -0,0 +1,10 @@
+package intermediateoops;
+
+public class Assignment2Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ intermediateoops/Assignment2Q3.java
@@ -0,0 +1,5 @@
+package intermediateoops;
+
+public class Assignment2Q3 {
+
+}
