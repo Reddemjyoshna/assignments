8abed25bb090d7190a2ab2f1f31d9c460eef103e
Reddemjyoshna <62939217+Reddemjyoshna@users.noreply.github.com>
2022-03-09 22:58:08 +0530

    Add files via upload

---
+++ generics/Employee.java
@@ -0,0 +1,20 @@
+package generics;
+
+public class Employee {
+	int id;
+    String name;
+    double salary;
+    String department;
+
+    public Employee(int id, String name, double salary, String department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+    
+    void displayDetails() {
+        System.out.println("ID: " + id + "      Name: " + name + "     Salary: " + salary + "       Department: " + department);
+    }
+
+}
