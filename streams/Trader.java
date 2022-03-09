b86d622360dbd1a63fa3c9eab6fe71fb4f4d8b15
Reddemjyoshna <62939217+Reddemjyoshna@users.noreply.github.com>
2022-03-09 23:06:41 +0530

    Add files via upload

---
+++ streams/Trader.java
@@ -0,0 +1,21 @@
+package streams;
+
+public class Trader {
+	String name;
+	String city;
+	
+	
+	public Trader(String name, String city) {
+		super();
+		this.name = name;
+		this.city = city;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Trader [name=" + name + ", city=" + city + "]";
+	}
+	
+
+}
---
+++ streams/Transaction.java
@@ -0,0 +1,22 @@
+package streams;
+
+public class Transaction {
+	Trader trader;
+	int year;
+	int value;
+	
+	public Transaction(Trader trader, int year, int value) {
+		super();
+		this.trader = trader;
+		this.year = year;
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() {
+		return "Transaction [trader=" + trader + ", year=" + year + ", value=" + value + "]";
+	}
+	
+	
+
+}
